6d076450de4d8c49390f8bb5f803d055d46670e8
Ivan <44531255+M81V4N@users.noreply.github.com>
2019-09-25 23:33:37 +0200

    Create NumberGrater.py

---
+++ math/NumberGrater.py
@@ -0,0 +1,55 @@
+#by M81V4N
+#split number on prime numbers
+import math
+num = int(input("num: "))
+print()
+containsPrimes = []
+currentNum = num
+dividers = []
+killTheInfiniteLoopOfDoom = False
+a = float(5)
+primes = [2, 3]
+#generate primes smaller than num
+while True:
+    #clear
+    search = 0
+    choosenOnes = []
+    root = math.sqrt(a)
+    while True:
+        calcFloat = float(a / primes[search])
+        calcInt = int(calcFloat)
+        if calcFloat == calcInt:
+            choosenOnes.append(primes[search])
+            break
+        search += 1
+        if root < primes[search]:
+            break
+    if len(choosenOnes) == 0:
+        primes.append(int(a))
+    if a >= num:
+      break
+    a += 2
+
+if primes[-1] == num:
+    print(str(num)+' | '+str(num))
+    print('PRIME NUMBER')
+    dividers.append(nim)
+    
+else:
+    while True:
+        for a in primes:
+            if currentNum/a == int(currentNum/a):
+                print(str(currentNum)+' | ' +str(a))
+                currentNum = int(currentNum/a)
+                dividers.append(a)
+                break
+                
+            if currentNum == 1:
+            	killTheInfiniteLoopOfDoom = True
+            	print("1 |")
+            	break
+        
+        if killTheInfiniteLoopOfDoom is True:
+            break
+             
+              
