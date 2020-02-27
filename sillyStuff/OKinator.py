c1c9fe91784ac589a5c9c389e33451be57f61bf1
Ivan <44531255+M81V4N@users.noreply.github.com>
2020-02-27 16:38:33 +0100

    Update OKinator.py

--- sillyStuff/OKinator.py
+++ sillyStuff/OKinator.py
@@ -1,1 +1,7 @@
-
+import string
+letterList = list(string.ascii_lowercase)
+text = input().lower()
+out = ""
+for char in text:
+	out += ((letterList.index(char)+1)*'ok').capitalize() if char in letterList else char
+print(out)
